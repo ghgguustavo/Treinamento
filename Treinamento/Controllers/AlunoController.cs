fa1d2a6efbb30fc9ddd9f344695e5a157c4503d1
SauloCandeira <saulocandeira@hotmail.com>
2021-03-11 12:02:32 -0300

    Aula 1

---
+++ Treinamento/Controllers/AlunoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Treinamento.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            string[] alunos = {
+                "José",
+                "Joao",
+                "Maria"
+            };
+
+            return View(alunos);
+        }
+    }
+}
